42622a4ff3a3f0149bfbeff1404ee37c4a2792ea
Francesco <med.dama@gmail.com>
2020-06-05 13:42:32 +0100

    Add and setup Pianotest

---
+++ src/test/java/instruments/PianoTest.java
@@ -0,0 +1,41 @@
+package instruments;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class PianoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getModel() {
+    }
+
+//    @Test
+//    public void getBrand() {
+//    }
+//
+//    @Test
+//    public void getMaterial() {
+//    }
+//
+//    @Test
+//    public void getType() {
+//    }
+//
+//    @Test
+//    public void getBuyingPrice() {
+//    }
+//
+//    @Test
+//    public void getSellingPrice() {
+//    }
+//
+//    @Test
+//    public void getOctaveNumber() {
+//    }
+}
