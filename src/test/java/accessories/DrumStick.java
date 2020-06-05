d5148ed262d023884dc2cc552cef28b04cc21de6
Francesco <med.dama@gmail.com>
2020-06-05 14:54:47 +0100

    Add DrumStick test

---
+++ src/test/java/accessories/DrumStick.java
@@ -0,0 +1,11 @@
+package accessories;
+
+public class DrumStick extends Accessory{
+
+    public DrumStick(String model, String brand, double sellingPrice, double buyingPrice) {
+        super(model, brand, sellingPrice, buyingPrice);
+    }
+
+
+
+}
---
+++ src/test/java/accessories/DrumStickTest.java
@@ -0,0 +1,29 @@
+package accessories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DrumStickTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getModel() {
+    }
+
+    @Test
+    public void getBrand() {
+    }
+
+    @Test
+    public void getSellingPrice() {
+    }
+
+    @Test
+    public void getBuyingPrice() {
+    }
+}
