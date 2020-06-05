5b38a2774e0e7ab10a3affc997df8684960380e2
Francesco <med.dama@gmail.com>
2020-06-05 13:45:43 +0100

    Add Pianotest getBrand

--- src/test/java/instruments/PianoTest.java
+++ src/test/java/instruments/PianoTest.java
@@ -3,16 +3,22 @@ package instruments;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 
 
 public class PianoTest {
 
+    Piano piano;
+
     @Before
     public void setUp() throws Exception {
+    piano = new Piano("2","Yamaha", "wood", InstrumentType.KEYBOARD, 2000, 4000,6);
+
     }
 
     @Test
     public void getModel() {
+        assertEquals(2, piano.getModel());
     }
 
 //    @Test
