2996187ae0fea95c0c4e0d9c30a9f995b0aa6d79
Francesco <med.dama@gmail.com>
2020-06-05 14:47:59 +0100

    Add GuitarString class

---
+++ src/main/java/accessories/GuitarStrings.java
@@ -0,0 +1,7 @@
+package accessories;
+
+public class GuitarStrings extends Accessory{
+    public GuitarStrings(String model, String brand, double sellingPrice, double buyingPrice) {
+        super(model, brand, sellingPrice, buyingPrice);
+    }
+}
