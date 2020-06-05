01b89d69511e007c3ffa66b3237a09d012d25a2d
Francesco <med.dama@gmail.com>
2020-06-05 15:10:40 +0100

    MusicSheets test passed

--- src/test/java/accessories/MusicSheetsTest.java
+++ src/test/java/accessories/MusicSheetsTest.java
@@ -3,6 +3,7 @@ package accessories;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 
 
 public class MusicSheetsTest {
@@ -11,26 +12,31 @@ public class MusicSheetsTest {
 
     @Before
     public void setUp() throws Exception {
-        musicSheets = new MusicSheets("3", "Music", 3.00,5.00);
+        musicSheets = new MusicSheets("3", "Music", 3.00,5.00,20);
     }
 
     @Test
     public void getModel() {
+        assertEquals("3", musicSheets.getModel());
     }
 
     @Test
     public void getBrand() {
+        assertEquals("Music", musicSheets.getBrand());
     }
 
     @Test
     public void getSellingPrice() {
+        assertEquals(5, musicSheets.getBuyingPrice(),0.01);
     }
 
     @Test
     public void getBuyingPrice() {
+        assertEquals(3, musicSheets.getSellingPrice(),0.01);
     }
 
     @Test
     public void getNumberOfPages() {
+        assertEquals(20,musicSheets.getNumberOfPages());
     }
 }
